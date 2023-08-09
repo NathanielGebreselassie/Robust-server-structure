77ead7559525ec8aa213ca036f6c6c6d2b3fa50b
Nathaniel Gebreselassie <130079278+NathanielGebreselassie@users.noreply.github.com>
2023-08-09 15:27:44 -0700

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const pastes = require("./data/pastes-data");
+const pastesRouter = require("./pastes/pastes.router");
+
+app.use(express.json());
+
+app.use("/pastes", pastesRouter); // Note: app.use
+
+// Not found handler
+app.use((request, response, next) => {
+  next(`Not found: ${request.originalUrl}`);
+});
+
+// Error handler
+app.use((error, req, res, next) => {
+  console.error(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
+});
+
+module.exports = app;
